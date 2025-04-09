3d70131967105b33c51d5c5adf34ed98cd58635f
Aishwaryajannu <aishwaryajannu17@gmail,com>
2025-04-09 12:20:46 +0530

    update innerhtml in tel

--- profiles/static/script.js
+++ profiles/static/script.js
@@ -15,7 +15,7 @@
         document.getElementById("profile-name").textContent = data.name;
         document.getElementById("profile-role").textContent = data.designation;
         document.getElementById("profile-email").innerHTML = `<a href="mailto:${data.email}">${data.email}</a>`;
-        document.getElementById("profile-phone").textContent = `<a href="tel:${data.phone}">${data.phone}</a>`;
+        document.getElementById("profile-phone").innerHTML = `<a href="tel:${data.phone}">${data.phone}</a>`;
         document.getElementById("profile-address").textContent = data.address;
         document.getElementById("profile-linkedin").innerHTML = `<a href="${data.linkedin}" target="_blank">${data.name}</a>`;
         document.getElementById("company-link").href = data.company_link;
